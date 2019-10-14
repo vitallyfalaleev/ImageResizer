5ffcc0b2bb789be276b480e02ede9f501bb366a6
Vitaliy <dante31.3d@gmail.com>
2019-10-14 14:41:39 +0300

    done registration

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,6 @@
+class V1::UsersController < ApplicationController
+  def index
+    @user = User.all
+    render json: @user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :v1 do
+    devise_scope :user do
+      post "signup", to: "registrations#create"
+      post "login", to: "sessions#create"
+    end
+    resources :users, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
